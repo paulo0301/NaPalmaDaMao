77d35e1cd4c52b7cf0d19e86ac8e9b744ebbe669
Paulo Daniel <pdanielcdo11@gmail.com>
2020-01-28 17:52:43 -0300

    modelos produzidos

---
+++ Modelos produzidos/modelos/modelo checkbox/js/verificar.js
@@ -0,0 +1,69 @@
+// GABARITO                 
+var R = ['b', 'd', 'a'];
+
+
+function alternativaMarcada(){
+    var alternativas = document.querySelectorAll("input[type='checkbox']");
+    for(let i = 0; i < alternativas.length; i++){
+        if(alternativas[i].checked){
+            return true;
+        }
+    }
+    return false;
+}
+
+var erradas = ['a', 'b', 'c', 'd', 'e'];
+
+function modificarVetor(){
+    for(let i = 0; i < R.length; i++){
+        var pos = erradas.indexOf(R[i]);
+        if(pos != -1){
+            erradas.splice(pos, 1);
+        }
+    }
+}
+modificarVetor();
+
+function verificar(){
+    var op = true;
+    for(let i = 0; i < R.length; i++){
+        alternativa = document.getElementById(R[i]);
+        if(alternativa.checked != true)op = false;
+    }
+    for(let i = 0; i < erradas.length; i++){
+        alternativa = document.getElementById(erradas[i]);
+        if(alternativa.checked == true)op = false;
+    }
+    return op;
+}
+
+var confirmar = document.getElementById('confirmar');
+
+confirmar.onclick = function(){
+    var a = document.getElementById("avaliacao");
+    if(alternativaMarcada() == true){
+        if(verificar() == true){
+            a.innerHTML = "Correto!";
+            a.classList.remove("letra-pequena");
+            a.classList.remove("errado");
+            a.classList.add("correto");
+        }
+        else{
+            a.innerHTML = "Errado...";
+            a.classList.remove("letra-pequena");
+            a.classList.remove("correto");
+            a.classList.add("errado");
+        }
+    }
+    else{
+        a.innerHTML = "Marque uma alternativa antes de confirmar.";
+        a.classList.remove("errado");
+        a.classList.remove("correto");
+        a.classList.add("letra-pequena");
+    }
+}
+
+ document.querySelectorAll("input[type='checkbox']").forEach(item => {item.addEventListener("click", funcao => {
+     var a = document.getElementById("avaliacao");
+     a.textContent = "";
+})});
